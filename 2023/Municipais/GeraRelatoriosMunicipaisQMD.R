74e4b3e3bb83434f0718b5f7dabc64c7b6f0f081
vcleto <vcleto@gmail.com>
2025-05-27 16:26:57 -0300

    - Ajusta script de geração dos arquivos qmd dos municípios para considerar que está no diretório raiz do projeto e não precisar configurar o diretório de trabalho para a pasta "Municipais". - Retira dos nomes dos municípios utilizados para nomear os arquivos qmd todos os caracateres que não forem alfanuméricos, porque além do espaço no nome do arquivo dar problema, também dá problema o apóstrofo em nomes de município como "d'água".

--- 2023/Municipais/GeraRelatoriosMunicipaisQMD.R
+++ 2023/Municipais/GeraRelatoriosMunicipaisQMD.R
@@ -1,21 +1,18 @@
-# Ajustar o caminho onde está o diretório "Municipais"
-setwd("caminho/Municipais")
-
 library("dplyr")
 library("readxl")
 
-dados_municipios <- read_excel("../../RELATORIO_DTB_BRASIL_MUNICIPIO.xls", skip = 6) %>%
+dados_municipios <- read_excel("../RELATORIO_DTB_BRASIL_MUNICIPIO.xls", skip = 6) %>%
                       mutate(`Código Município` = substr(`Código Município Completo`,1,6))
 
 gera_qmd <- function(municipio) {
   nome_municipio <- dados_municipios$Nome_Município[dados_municipios$`Código Município`==municipio]
   relatorio_municipio_qmd = knitr::knit_expand(
-    'Relatório Municipal 2023.qmd',
+    'Municipais/Relatório Municipal 2023.qmd',
     municipio = municipio, nome_municipio = nome_municipio
   )
   uf_municipio <- gsub(" ", "_", dados_municipios$Nome_UF[dados_municipios$`Código Município`==municipio])
-  dir.create(uf_municipio, showWarnings = FALSE)
-  writeLines(relatorio_municipio_qmd, paste0(uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2023-', uf_municipio, '-', gsub(" ", "_", nome_municipio), '.qmd'))
+  dir.create(paste0('Municipais/', uf_municipio), showWarnings = FALSE)
+  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2023-', uf_municipio, '-', gsub('[^[:alnum:]]', "_", nome_municipio), '.qmd'))
 }
 
 for (municipio in dados_municipios$`Código Município`) {
