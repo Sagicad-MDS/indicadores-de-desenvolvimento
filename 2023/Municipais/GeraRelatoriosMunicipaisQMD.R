8744019940e236aa6ab604432423ce50187b3e7e
vcleto <vcleto@gmail.com>
2025-05-27 18:12:17 -0300

    Em vez de substituir o apóstrofo por underline nos nomes dos arquivos qmd dos municípios, apenas retira o apóstrofo, mais por preciosismo... (d'água fica dágua em vez de d_água).

--- 2023/Municipais/GeraRelatoriosMunicipaisQMD.R
+++ 2023/Municipais/GeraRelatoriosMunicipaisQMD.R
@@ -10,9 +10,10 @@ gera_qmd <- function(municipio) {
     'Municipais/Relatório Municipal 2023.qmd',
     municipio = municipio, nome_municipio = nome_municipio
   )
+  nome_municipio <- gsub("'", "", gsub(" ", "_", nome_municipio))
   uf_municipio <- gsub(" ", "_", dados_municipios$Nome_UF[dados_municipios$`Código Município`==municipio])
   dir.create(paste0('Municipais/', uf_municipio), showWarnings = FALSE)
-  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2023-', uf_municipio, '-', gsub('[^[:alnum:]-]', "_", nome_municipio), '.qmd'))
+  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2023-', uf_municipio, '-', nome_municipio, '.qmd'))
 }
 
 for (municipio in dados_municipios$`Código Município`) {
