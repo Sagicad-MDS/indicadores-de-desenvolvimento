199887f12e32e3d0511a227239a46fedbee42ba9
vcleto <vcleto@gmail.com>
2025-12-25 09:00:21 -0300

    - Unifica código comum nos arquivos _carregamento.R, _funcoes.R e _setup.R - Acrescenta parágrafo indicando a página com a fonte dos dados - Acrescenta links para as páginas da Wiki relativas a cada indicador e a cada dimensão de cada indicador. - Mudança de sintaxe automática feita pelo RStudio. - Utilização dos arquivos de dados originais, conforme disponibilizados, sem ajustes. - Correções nos nomes de variáveis utilizadas para corresponder ao conteúdo dos arquivos de dados originais, como disponibilizados, sem ajustes.

---
+++ 2023/_setup.R
@@ -0,0 +1,13 @@
+library("dplyr")
+library("stringr")
+library("forcats")
+library("tidyr")
+library("scales")
+library("readxl")
+library("readr")
+library("ggrepel")
+library("readODS")
+library("knitr")
+library("janitor")
+
+color.map.option = "E" #"cividis"
