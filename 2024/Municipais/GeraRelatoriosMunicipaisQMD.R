21ce3284ef58ccf84f9b79a8233e99013897a93e
Valdson <vcleto@gmail.com>
2026-01-03 22:06:46 -0300

    Atualiza relatórios municipais para 2024

--- 2024/Municipais/GeraRelatoriosMunicipaisQMD.R
+++ 2024/Municipais/GeraRelatoriosMunicipaisQMD.R
@@ -7,13 +7,13 @@ dados_municipios <- read_excel("../RELATORIO_DTB_BRASIL_MUNICIPIO.xls", skip = 6
 gera_qmd <- function(municipio) {
   nome_municipio <- dados_municipios$Nome_Município[dados_municipios$`Código Município`==municipio]
   relatorio_municipio_qmd = knitr::knit_expand(
-    'Municipais/Relatório Municipal 2023.qmd',
+    'Municipais/Relatório Municipal 2024.qmd',
     municipio = municipio, nome_municipio = nome_municipio
   )
   nome_municipio <- gsub("'", "", gsub(" ", "_", nome_municipio))
   uf_municipio <- gsub(" ", "_", dados_municipios$Nome_UF[dados_municipios$`Código Município`==municipio])
   dir.create(paste0('Municipais/', uf_municipio), showWarnings = FALSE)
-  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2023-', uf_municipio, '-', nome_municipio, '.qmd'))
+  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2024-', uf_municipio, '-', nome_municipio, '.qmd'))
 }
 
 for (municipio in dados_municipios$`Código Município`) {
