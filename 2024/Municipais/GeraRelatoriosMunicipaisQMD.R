c875586f3d9c9ae71693c7012e15c9dc95e92d60
Valdson <vcleto@gmail.com>
2026-04-15 22:38:03 -0300

    Cria um arquivo index.qmd para cada UF para determinar o nome (e a correta formatação) do nome do Estado no índice para os relatórios municipais (na geração automática o Quarto coloca todas as palavras com a primeira letra em maiúsculo, inclusive as preposições, ficando, por exemplo, Mato Grosso Do Sul).

--- 2024/Municipais/GeraRelatoriosMunicipaisQMD.R
+++ 2024/Municipais/GeraRelatoriosMunicipaisQMD.R
@@ -6,15 +6,17 @@ dados_municipios <- read_excel("../RELATORIO_DTB_BRASIL_MUNICIPIO.xls", skip = 6
 
 gera_qmd <- function(municipio) {
   nome_municipio <- dados_municipios$Nome_Município[dados_municipios$`Código Município`==municipio]
+  uf_municipio <- dados_municipios$Nome_UF[dados_municipios$`Código Município`==municipio]
   relatorio_municipio_qmd = knitr::knit_expand(
     'Municipais/Relatório Municipal 2024.qmd',
     municipio = municipio, nome_municipio = nome_municipio,
     delim = c("[[", "]]")
   )
   nome_municipio <- gsub("'", "", gsub(" ", "_", nome_municipio))
-  uf_municipio <- gsub(" ", "_", dados_municipios$Nome_UF[dados_municipios$`Código Município`==municipio])
-  dir.create(paste0('Municipais/', uf_municipio), showWarnings = FALSE)
-  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio, '/Relatório_Indicadores_Censo_SUAS_2024-', uf_municipio, '-', nome_municipio, '.qmd'))
+  uf_municipio_dir <- gsub(" ", "_", uf_municipio)
+  dir.create(paste0('Municipais/', uf_municipio_dir), showWarnings = FALSE)
+  writeLines(sprintf("---\ntitle: %s\n---", uf_municipio), paste0('Municipais/', uf_municipio_dir, '/index.qmd'))
+  writeLines(relatorio_municipio_qmd, paste0('Municipais/', uf_municipio_dir, '/Relatório_Indicadores_Censo_SUAS_2024-', uf_municipio_dir, '-', nome_municipio, '.qmd'))
 }
 
 for (municipio in dados_municipios$`Código Município`) {
